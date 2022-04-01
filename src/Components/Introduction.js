18cf9297f25e8b13ea1b00c330955eec7a5e7f5c
ArpanaMishra94 <arpanamishra394@gmail.com>
2022-04-01 10:55:36 +0530

    Initial commit

---
+++ src/Components/Introduction.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import FeetWingsNav from './FeetWingsNav';
+import JobRoleDesc from './JobRoleDesc';
+export default function Introduction() {
+    return(
+        <div className = "introduction">
+              <JobRoleDesc/>
+        </div>
+    );
+}
